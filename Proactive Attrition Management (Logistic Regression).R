f199b7978cf4f2d3e8c2efee464b53ed9d6424b6
arch44 <arun.chaudhry44@gmail.com>
2016-12-12 10:00:35 +0530

    used mice for imputation; used caret to build and estimate performance

--- Proactive Attrition Management (Logistic Regression).R
+++ Proactive Attrition Management (Logistic Regression).R
@@ -1,232 +1,317 @@
-# Objective: Using Logistic Regression to create a classifier to Identify potential 
-# churners from Proactive Attrition Management data for 70,000 customers.
+# Essential libraries
+library(caret)
+library(e1071)
+library(corrplot)
+library(MASS)
+library(dplyr)
+library(mice)
+library(ROCR)
 
-
-# -----------------------------Importing the Data
+# Importing the data
 pam <- read.csv("Proactive Attrition Management-Logistic Regression Case Study.csv")
 
 # Exploring the data
 str(pam)
 summary(pam)
 
-length(which(pam$INCOME==0)) #17750
-length(which(pam$SETPRC==0)) #40249
+#################################################
+# Fixing Variables' Data types
+#################################################
 
-#-----------------------------Preparing the Data
+# Convert all variables to Numeric
+for(i in 1:ncol(pam)){
+  pam[,i] <- as.numeric(pam[,i])
+}
+str(pam)
 
-# changing labels for NA in 2 predictors
-pam$INCOME[pam$INCOME == 0] <- NA
-pam$SETPRC[pam$SETPRC == 0] <- NA
-
-summary(pam$INCOME)
-summary(pam$SETPRC)
-
-# Changing data types for categorical predictors
-fac <- c(22,33:64,67,68,70,72,74,76:78)
-
+# Convert categorical features to factors
+fac <- c(22,26,33:68,72,76)
 for(i in fac){
   pam[,i] <- as.factor(pam[,i])
 }
-
 str(pam)
 
-# Removing customer id variable
-which(names(pam) == "CUSTOMER")
-pam <- pam[,-30]
+#################################################
+# Data Preparation
+#################################################
 
-#---------------------------------Examining Skewness of numerical predictors
+# Missing values in income and setprc
+length(which(pam$INCOME==0)) #17750
+length(which(pam$SETPRC==0)) #40249
 
-# Separate out factors from numerical/interger predictors
-fac <- which(sapply(pam, is.factor))
-pam.num <- pam[,-fac]
-str(pam.num)
+pam$INCOME[pam$INCOME == 0] <- NA
+pam$SETPRC[pam$SETPRC == 0] <- NA
 
-# Calculating skewness for all numerical predictors using apply
-library(e1071)
-apply(pam.num, 2, skewness, na.rm = TRUE)
-summary(pam.num)
+# Removing redundant features
+pam <- pam %>% select(-INCMISS, -SETPRCM, -CUSTOMER, -MARRYNO, -NEWCELLN, -CHURNDEP)
 
-    # Most predictors are right skewed, and include negative or zero values
-    # therefore not suitable for log transformation.
+# Save CALIBRAT to a vector and remove it
 
-#---------------------------------Examining missing values
+CALIBRAT <- pam$CALIBRAT
+pam <- pam %>% select(-CALIBRAT)
 
-# Number of missing values in all variables
-apply(pam, 2, function(x){sum(is.na(x))})
-mean(is.na(pam$INCOME))
-mean(is.na(pam$SETPRC))
-mean(is.na(pam$CHURNDEP))
+#################################################
+# Separate out Predictors and Outcome Variable
+#################################################
+pamPred <- pam %>% select(-CHURN)
+pamOut <- pam %>% select(CHURN)
+
+#################################################
+# Dealing with missing Values
+#################################################
+
+# Number of Missing values
+numMissing <- apply(pam, 2, function(x){sum(is.na(x))})
+numMissing
+
+# Removing SETPRC variable
+pamPred <- pamPred %>% select(-SETPRC)
+
+# Imputing values
+
+n <- which(names(pamPred) == "CSA")
+imputed <- complete(mice(pamPred[,-n]))
+str(imputed)
+
+# Replacing missing values
+
+pamPred$REVENUE <- imputed$REVENUE
+pamPred$MOU <- imputed$MOU
+pamPred$RECCHRGE <- imputed$RECCHRGE
+pamPred$DIRECTAS <- imputed$DIRECTAS
+pamPred$OVERAGE <- imputed$OVERAGE
+pamPred$ROAM <- imputed$ROAM
+pamPred$CHANGEM <- imputed$CHANGEM
+pamPred$CHANGER <- imputed$CHANGER
+pamPred$PHONES <- imputed$PHONES
+pamPred$MODELS <- imputed$MODELS
+pamPred$EQPDAYS <- imputed$EQPDAYS
+pamPred$AGE1 <- imputed$AGE1
+pamPred$AGE2 <- imputed$AGE2
+pamPred$INCOME <- imputed$INCOME
+
+# Write this data to a file
+
+pamImputed <- cbind(pamPred, CALIBRAT)
+pamImputed <- cbind(pamImputed, pamOut)
+write.csv(pamImputed, "pamImputed.csv", row.names = FALSE)
+
+#################################################
+# Fixing Skewed Predictors
+#################################################
+
+# Determining the skewness of continuous predictors
+pamContPred <- pamPred[,!sapply(pamPred, is.factor)]
+
+skewness <- apply(pamContPred, 2, skewness)
+summary(skewness)
+
+# Using Box-Cox Transformation to transform skewed predictors
+boxcoxtrans <- preProcess(pamPred, method = "BoxCox")
+boxcoxtrans
+
+# 27 continuous predictors were not transformed due to zero or negative values 
+# 5 predictors were transformed with lambda values between -1 and 1 
+
+pamPred <- predict(boxcoxtrans, pamPred)
+
+#################################################
+# Two sets of Predictors: fullSet and reducedSet
+#################################################
+
+# full set: pamPred
+fullSet <- cbind(pamPred, CHURN = pamOut)
+
+#################################################
+# Preparing reducedSet
+#################################################
+
+# Checking for multicollinearity
+
+# Using PCA to detect multi-collinearity
+pamContPred <- pamPred[,!sapply(pamPred, is.factor)]
+pcaObject <- prcomp(pamContPred, center = TRUE, scale. = TRUE)
+ls(pcaObject)
+percentVariance <- pcaObject$sdev^2/sum(pcaObject$sdev^2)*100
+percentVariance[1:3]
+plot(percentVariance, xlab = "Components", ylab = "Cumulative Percent of Variance", type = "b")
+
+# The amount of variability summarized by components drop sharply, 
+# with no one component accounting for more than 27% of the variance. 
+# This profile indicates that the structure of the data is contained 
+# in a much smaller number of dimensions than the number of dimensions of the original space. 
+
+# This is often due to a large number of collinearities among the predictors.
+
+# Correlation between Predictors
+correlation <- cor(pamContPred)
+corrplot(correlation, order = "hclust")
+
+# There are many strong positive correlations (indicated by the large, dark blue circles)
+
+# Removing Strongly piecewise-correlated predictors
+pwcp <- findCorrelation(correlation, cutoff = 0.8)
+names(pamContPred[pwcp])
+pamContPred <- pamContPred[,-pwcp]
+
+# Merging with Categorical predictors
+pamCatPred <- pamPred[,sapply(pamPred, is.factor)]
+
+reducedSet <- cbind(pamContPred, pamCatPred)
+
+# Removing Near-Zero Variance Predictors
+nzvp <- nearZeroVar(reducedSet)
+names(reducedSet[nzvp])
+reducedSet <- reducedSet[,-nzvp]
+
+reducedSet <- cbind(reducedSet, CHURN = pamOut)
+
+fullSet$CHURN <- factor(fullSet$CHURN, labels = c("No", "Yes"))
+reducedSet$CHURN <- factor(reducedSet$CHURN, labels = c("No", "Yes"))
+
+#################################################
+# Splitting the data into Training and Test set
+#################################################
+
+# adding CALIBRAT
+fullSetC <- cbind(fullSet, CALIBRAT)
+reducedSetC <- cbind(reducedSet, CALIBRAT)
+
+fullCal <- fullSetC %>% filter(CALIBRAT == 1) %>% select(-CALIBRAT)
+fullVal <- fullSetC %>% filter(CALIBRAT == 0) %>% select(-CALIBRAT)
+
+redCal <- reducedSetC %>% filter(CALIBRAT == 1) %>% select(-CALIBRAT)
+redVal <- reducedSetC %>% filter(CALIBRAT == 0) %>% select(-CALIBRAT)
 
 
-    # The first 8 columns are missing the values of the same observations. 
-    # additionally, 286 more observations are missing from 7th and 8th predictors
-    # the AGE variables are missing values for 1244 observations. 
+#################################################
+# Model Training
+#################################################
 
-#----------------------------Removing predictors and keeping complete cases
+levels(fullCal$CHURN)
+# "No" "Yes"
+# The glm function treats the second factor level as the event of interest.
+# so, the probability output from glm function will give the probability of CHURN
 
-# Removing INCOME, SETPRC and CHURNDEP
-library(dplyr)
-pam <- pam %>% select(-CHURNDEP, -INCOME, -SETPRC, -INCMISS, -SETPRCM)
-str(pam)
+######################### Model 1: Baseline Model
+prop.table(table(fullVal$CHURN))
+# Accuracy: 0.9803
 
-# removing INCOME and SETPRC predictors and their associated predictors
-pam <- pam %>% select(-INCOME, -SETPRC, -INCMISS, -SETPRCM)
-str(pam)
+######################### Model 2: All Predictors
+glmAllPredictors <- glm(CHURN~., data = fullCal, family = binomial)
+summary(glmAllPredictors)
+# AIC: 54400
 
-# keeping all the complete cases
-pam <- pam[complete.cases(pam),]
-str(pam)
+######################### Model 3: Reduced Number of Predictors
+glmRedPred <- glm(CHURN~., data = reducedCal, family = binomial)
+summary(glmRedPred)
+# AIC: 54397
 
-#----------------------------------------Examining predictors
+######################### Model 4: Using Stepwise Selection to select the Best Model
 
-#--------------------- near zero variance predictors [pam1]
-nz <- nearZeroVar(pam)
-nz
-names(pam[,nz])
+# Model with all predictors except CSA 
+glmAllFull2 <- glm(CHURN~.-CSA, data = fullCal, family = binomial)
+glmAllFull2
+# AIC: 53920
 
-pam1 <- pam[,-nz]
-str(pam1)
+# Using Step function to find the best model
+glmFit1 <- stepAIC(glmAllFull2, direction = "both")
+glmFit1
+# AIC: 53880
 
-#--------------------- correlated predictors
-
-# numeric/integer predictors
-
-fac <- which(sapply(pam, is.factor))
-pam.npred <- pam[,-fac]
-str(pam.npred)
-
-# converting interger predictors to numeric
-for(i in 1:ncol(pam.npred)){
-  pam.npred[,i] <- as.numeric(pam.npred[,i])
-}
-
-str(pam.npred)
-
-# correlations
-correlations <- cor(pam.npred)
-corrplot(correlations, order = "hclust")
-
-# numeric variables with pairwise correlation exceeding 0.70
-cp <-findCorrelation(correlations, cutoff = 0.70)
-cp
-ncp <- names(pam.npred[,cp])
-ncp
-
-#------------------------------------Removing correlated predictors
-
-pam.ncp <- pam %>% select(-MOU,-PEAKVCE, -OPEAKVCE,-OUTCALLS,-REVENUE,-DROPBLK,-PHONES,-UNIQSUBS,-RETACCPT)
-str(pam.ncp)
-
-#-----------------------------Splitting the data pam.ncp
-
-pam.c1 <- pam.ncp %>% filter(CALIBRAT == "1")
-pam.v1 <- pam.ncp %>% filter(CALIBRAT == "0")
-
-str(pam.c1)
-summary(pam.c1)
-
-pam.c1 <- pam.c1 %>% select(-CALIBRAT)
-pam.v1 <- pam.v1 %>% select(-CALIBRAT)
-
-pam.c1.pred <- pam.c1 %>% select(-CHURN)
-pam.c1.label <- pam.c1 %>% select(CHURN)
-
-pam.v1.pred <- pam.v1 %>% select(-CHURN)
-pam.v1.label <- pam.v1 %>% select(CHURN)
-
-#---------------------------Baseline model
-
-prop.table(table(pam.c1$CHURN))
-
-# always predicting 0 i.e. no churn
-# 50.2% accuracy of the baseline model
-
-#-------------------Training Logistic Regression Classifier
-
-pam.null.model <- glm(CHURN~1, data = pam.c1, family = binomial)
-
-pam.model1 <- glm(CHURN~., data = pam.c1, family = binomial)
-pred1 <- predict(pam.model1, type = "response")
-table(pam.c1$CHURN, pred1 >= 0.5)
-(11862+11403)/nrow(pam.c1)
-
-# better than baseline model
-
-# improving the model: feature selection
-library(MASS)
-#pam.model2 <- step(pam.null.model, scope = formula(pam.model1), direction = "forward", k = 2)
-
-# removing CSA from the main data set
-summary(pam.model1) #aic 53077
-pam.model3 <- glm(CHURN~.-CSA, data = pam.c1, family = binomial)
-pam.model4 <- step(pam.null.model, scope = formula(pam.model3), direction = "forward", k = 2)
+# Adding CSA variable to the final model
+glmFit2 <- glm(formula = CHURN ~ REVENUE + MOU + RECCHRGE + OVERAGE + ROAM + 
+      CHANGEM + CHANGER + DROPVCE + BLCKVCE + UNANSVCE + CUSTCARE + 
+      THREEWAY + OUTCALLS + INCALLS + PEAKVCE + MONTHS + UNIQSUBS + 
+      ACTVSUBS + PHONES + MODELS + EQPDAYS + AGE1 + CHILDREN + 
+      CREDITA + CREDITC + CREDITDE + CREDITGY + PRIZMRUR + PRIZMUB + 
+      REFURB + WEBCAP + OCCHMKR + MARRYUN + MAILRES + RETCALLS + 
+      RETACCPT + NEWCELLY + MCYCLE + CREDITAD + CSA, family = binomial, 
+      data = fullCal)
+glmFit2
+# AIC: 54360
 
 
-#best model we get, add csa if aic improves keep it.
-pam.model5 <- glm(CHURN ~ EQPDAYS + RETCALL + CREDITDE + AGE1 + REFURB + MONTHS + 
-                    CHANGEM + CHANGER + RECCHRGE + OVERAGE + ROAM + CREDITC + 
-                    MOUREC + MODELS + WEBCAP + MAILRES + CHILDREN + DROPVCE + 
-                    INCALLS + CUSTCARE + NEWCELLY + PRIZMUB + THREEWAY + MARRYUN + 
-                    BLCKVCE + CREDITAD + CREDITZ + OCCHMKR + MCYCLE + REFER + CSA,
-                  data = pam.c1, family = binomial)
-summary(pam.model5) #53046 vs. 52553.43 from model4
+##################################################
+# Choosing the Model
+##################################################
 
-# ---------------------------------model4 gives the lowest AIC
+# Final Model
+model <- glmFit1
 
-#--------------------------------------choosing the threshold
-# cost of making a FN > cost of making a FP
-# i.e. we have to maximize the value of sensitivity and specificity
+# Cross Validation Estimate of Performance
 
-#----------------------------------Using ROC curve to choose a threshold
-install.packages("ROCR")
-library(ROCR)
+# Using 10-fold Cross Validation
+ctrl <- trainControl(method = "cv", number =  10,
+                     classProbs = TRUE)
 
-ROCRpred <- prediction(pred2, pam.c1$CHURN)
+trFit <- train(CHURN ~ REVENUE + MOU + RECCHRGE + OVERAGE + ROAM + 
+                 CHANGEM + CHANGER + DROPVCE + BLCKVCE + UNANSVCE + CUSTCARE + 
+                 THREEWAY + OUTCALLS + INCALLS + PEAKVCE + MONTHS + UNIQSUBS + 
+                 ACTVSUBS + PHONES + MODELS + EQPDAYS + AGE1 + CHILDREN + 
+                 CREDITA + CREDITC + CREDITDE + CREDITGY + PRIZMRUR + PRIZMUB + 
+                 REFURB + WEBCAP + OCCHMKR + MARRYUN + MAILRES + RETCALLS + 
+                 RETACCPT + NEWCELLY + MCYCLE + CREDITAD, data = fullCalCSA,
+                 method = "glm", trControl = ctrl)
+trFit
+
+# Accuracy  Kappa   
+# 0.583775  0.16755
+
+##################################################
+# Select optimal threshold setting using ROC Curve
+##################################################
+
+ROCRpred <- prediction(glmPred, fullVal$CHURN)
 ROCRperf <- performance(ROCRpred, "sens", "spec")
-plot(ROCRperf)
+plot(ROCRperf, colorize = TRUE)
 
-ROCRperf@alpha.values[[1]][which.max(ROCRperf@x.values[[1]]+ROCRperf@y.values[[1]])]
-# 0.4770975
-max(ROCRperf@x.values[[1]]+ROCRperf@y.values[[1]])
-# 1.162105
+##################################################
+# Choosing the Threshold to make Class Predictions
+##################################################
 
-# The highest sensitivity plus specificity is achieved in this case when you predict 
-# the positive outcome when the predicted probability exceeds 0.477 and predict the 
-# negative outcome when the predicted probability does not exceed 0.477. 
-# This yield a sensitivity plus specificity value of 1.16
+# using 0.23 as to correctly predict all the churners in the test set. 
 
-#-----------------------------prediction over the training data
-pred2 <- predict(pam.model4, type = "response")
+##################################################
+# Make final predictions over Validation set
+##################################################
 
-# confusion matrix
-library(caret)
-cm <- table(pred2 > 0.477, pam.c1$CHURN)
-cm
-str(cm)
-dimnames(cm)[[1]] = c("0","1")
+glmPred <- predict(model, newdata = fullVal, type = "response")
+
+# Confusion matrix
+cm1 <- table(Prediction = glmPred > 0.23, Observed = fullVal$CHURN)
+dimnames(cm1)[[1]] = c("No","Yes")
+cm <- confusionMatrix(cm1)
 cm
 
-confusionMatrix(cm)
+##################################################
+# Calculate AUC
+##################################################
 
-
-#------------------------------prediction on the validation set
-pred.val <- predict(pam.model4, newdata = pam.v1, type = "response")
-
-# confusion matrix
-cm.val <- table(pred.val > 0.477, pam.v1$CHURN)
-cm.val 
-str(cm.val)
-dimnames(cm.val)[[1]] = c("0","1")
-cm.val
-confusionMatrix(cm.val)
-
-#calculating auc
-ROCRpredVal <- prediction(pred.val, pam.v1$CHURN)
+ROCRpredVal <- prediction(glmPred, fullVal$CHURN)
 auc = as.numeric(performance(ROCRpredVal, "auc")@y.values)
-auc #62%
-  
+auc 
+
+# 0.61
 
 # Interpretation of AUC
+
 # Given a random customer from the dataset who actually churned, and 
 # a random customer from the dataset who actually didn't churn,
 # 62 % of the time our model will classify which is which correctly. 
+
+#################################################
+# Determining variable imortance
+################################################# 
+
+importance <- varImp(trFit, scale=FALSE)
+
+# summarize importance
+print(importance)
+
+# plot importance
+plot(importance)
+
+
+
+
